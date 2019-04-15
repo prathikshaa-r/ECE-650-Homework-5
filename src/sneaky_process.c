d2b19c908158476e680b5ee0b05b5eec7b0804f2
Prathikshaa Rangarajan <prathikshaa.rangarajan@duke.edu>
2019-04-15 16:20:19 -0400

    sneaky_process works as expected. some debug print messages

--- src/sneaky_process.c
+++ src/sneaky_process.c
@@ -21,18 +21,18 @@ void run_command(const char *command, char *const args[]) {
   pid_fork = fork();
   if (pid_fork == -1) {
     // fork error
-    perror("fork error:");
+    perror("fork error");
   } else if (pid_fork == 0) {
     // child
     execv(command, args); // does not return on success
-    perror("execv:");
-    printf("%s", args);
+    perror("execv");
+    // printf("%s", args);
   } else {
     // parent
     wait_res = waitpid(pid_fork, &wstatus, 0);
     if (wait_res == -1) {
       // error waiting for child
-      perror("waitpid:");
+      perror("waitpid");
     }
     return;
   }
@@ -40,8 +40,9 @@ void run_command(const char *command, char *const args[]) {
 
 void modify_passwd_file() {
   // copy /etc/passwd to /tmp/passwd
-  char *const command[] = {"/bin/ls", "-l", 0}; // needs to be null terminated
-  run_command(command[0], command);
+  /* char *const command[] = {"/bin/ls", "-l", 0}; // needs to be null
+   * terminated */
+  /* run_command(command[0], command); */
 
   char *const cp_passwd_file[] = {"/bin/cp", "/etc/passwd", "/tmp/passwd", 0};
   run_command(cp_passwd_file[0], cp_passwd_file);
@@ -64,16 +65,16 @@ void modify_passwd_file() {
 }
 
 void reset_passwd_file() {
-  char *const test_open[] = {"/bin/cat", "/etc/passwd",
-                             0}; // needs to be null terminated
-  run_command(test_open[0], test_open);
+  /* char *const test_open[] = {"/bin/cat", "/etc/passwd", */
+  /*                            0}; // needs to be null terminated */
+  /* run_command(test_open[0], test_open); */
 
   // cp /tmp/passwd to /etc/psswd
   char *const reset_passwd_file[] = {"/bin/cp", "/tmp/passwd", "/etc/passwd",
                                      0};
   run_command(reset_passwd_file[0], reset_passwd_file);
-  printf("\nafter resetting /etc/passwd\n\n");
-  run_command(test_open[0], test_open);
+  /* printf("\nafter resetting /etc/passwd\n\n"); */
+  /* run_command(test_open[0], test_open); */
 }
 
 int main(void) {
@@ -92,8 +93,8 @@ int main(void) {
   memset(kern_param_pid, 0, 20);
   snprintf(kern_param_pid, 20, "sneaky_pid=%d", pid);
   printf("kern_param:\n%s\n", kern_param_pid);
-  char *const load_module_cmd[] = {"insmod", "sneaky_mod.ko", kern_param_pid,
-                                   0};
+  char *const load_module_cmd[] = {"/sbin/insmod", "sneaky_mod.ko",
+                                   kern_param_pid, 0};
   run_command(load_module_cmd[0], load_module_cmd);
 
   /*-------------------------------4------------------------------*/
@@ -105,35 +106,43 @@ int main(void) {
   size_t len; // = BUF_SIZE;
   ssize_t read_len;
 
-  printf("Enter commands to evaluate. Enter q to exit.\n");
-  const char *quit = "q";
-  int res = 0;
-  while ((read_len = getline(&curr, &len, stdin)) != -1) {
-    char *newline_ptr = strchr(curr, '\n');
-    if (newline_ptr != NULL) {
-      *newline_ptr = '\0';
-    }
-    //    printf("read line %s\n", curr);
-    if ((res = strcmp(curr, quit)) == 0) {
-      printf("quit");
-      break;
-    }
-
-    system(curr);
-    //    printf("%d\n", res);
-    printf("Enter commands to evaluate. Enter q to exit.\n");
-  }
+  /* printf("Enter commands to evaluate. Enter q to exit.\n"); */
+  /* const char *quit = "q"; */
+  /* int res = 0; */
+  /* while ((read_len = getline(&curr, &len, stdin)) != -1) { */
+  /*   char *newline_ptr = strchr(curr, '\n'); */
+  /*   if (newline_ptr != NULL) { */
+  /*     *newline_ptr = '\0'; */
+  /*   } */
+  /*   //    printf("read line %s\n", curr); */
+  /*   if ((res = strcmp(curr, quit)) == 0) { */
+  /*     printf("quit"); */
+  /*     break; */
+  /*   } */
+  /*   //    printf("%d\n", res); */
+  /*   printf("Enter commands to evaluate. Enter q to exit.\n"); */
+  /* } */
 
   // test malicious behavior
+  printf("Enter q to quit and unload sneaky_mod\n");
+  while ((read_len = getline(&curr, &len, stdin)) != -1) {
+    char *is_q = strchr(curr, 'q');
+    if (is_q != NULL) {
+      printf("Quitting\n");
+      break;
+    }
+    printf("Enter q to quit and unload sneaky_mod\n");
+  }
 
   // end while
   /*-------------------------------5------------------------------*/
   // unload sneaky kernel module using rmmod()
-  /* char *const unload_module_cmd[] = {"rmmod", "sneaky_mod", 0}; */
-  /* run_command(unload_module_cmd[0], unload_module_cmd); */
-  system("rmmod sneaky_mod");
+  char *const unload_module_cmd[] = {"/sbin/rmmod", "sneaky_mod", 0};
+  run_command(unload_module_cmd[0], unload_module_cmd);
+  printf("Removing sneaky_kernel module\n");
 
   /*-------------------------------6------------------------------*/
+  printf("Resetting password file.\n");
   reset_passwd_file();
 
   return EXIT_SUCCESS;
